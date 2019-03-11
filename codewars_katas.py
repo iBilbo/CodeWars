e6e221f58a5342523892fa4412517e72c4466766
iBilbo <35370175+iBilbo@users.noreply.github.com>
2019-03-11 13:00:59 +0000

    Update codewars_katas.py

--- codewars_katas.py
+++ codewars_katas.py
@@ -30,4 +30,59 @@ def alphabet_position(text):
     else:
         return True
  
+"""
+kyu 8. INSTRUCTIONS: ou might know some pretty large perfect squares. But what about the NEXT one?
+Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+If the parameter is itself not a perfect square, than -1 should be returned. You may assume the parameter is positive. Examples:
+findNextSquare(121) --> returns 144
+findNextSquare(625) --> returns 676
+findNextSquare(114) --> returns -1 since 114 is not a perfect
+"""
+import math
+def find_next_square(sq):
+    if math.sqrt(sq).is_integer():
+        return int((math.sqrt(sq) + 1) ** 2)
+    else:
+        return -1
+
+"""
+7kyu. INSTRUCTIONS: 
+Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters,
+
+    each taken only once - coming from s1 or s2.
+
+Examples:
+
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+"""
+
+def longest(s1, s2):
+    longest_string=""
+    uniqs1 = ""
+    uniqs2 = ""
+    for letter in s1:
+        if letter in uniqs1:
+            continue
+        else:
+            uniqs1 += letter
+    
+    for letter in s2:
+        if letter in uniqs2:
+            continue
+        else:
+            uniqs2 += letter
+            
+    for letter in uniqs2:
+        if letter in uniqs1:
+            continue
+        else:
+            uniqs1 += letter
+
+    return "".join(sorted(uniqs1))
+ 
  
