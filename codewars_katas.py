94bbe58c9d6f988db9c8a37fff4f06f6ad1ec80d
iBilbo <35370175+iBilbo@users.noreply.github.com>
2019-03-11 16:51:52 +0000

    Update codewars_katas.py

--- codewars_katas.py
+++ codewars_katas.py
@@ -132,4 +132,15 @@ def likes(names):
          return names[0] + ", " + names[1] + " and " + names[2] + " like this"
     else:
         return names[0] + ", " + names[1] + " and " + str(len(names) - 2) + " others like this"
+
+    #Other options
+def likes(names):
+    n = len(names)
+    return {
+        0: 'no one likes this',
+        1: '{} likes this', 
+        2: '{} and {} like this', 
+        3: '{}, {} and {} like this', 
+        4: '{}, {} and {others} others like this'
+    }[min(4, n)].format(*names[:3], others=n-2)
  
