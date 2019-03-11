ae747b8216a21ee8b5aca5ddd6f25eb65fe68980
iBilbo <35370175+iBilbo@users.noreply.github.com>
2019-03-11 12:04:15 +0000

    Create codewars_katas.py

---
+++ codewars_katas.py
@@ -0,0 +1,33 @@
+"""
+kyu 8. INSTRUCTIONS. Welcome. In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+If anything in the text isn't a letter, ignore it and don't return it. "a" = 1, "b" = 2, etc.
+"""
+
+def alphabet_position(text):
+    my_dict = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10, "k": 11, "l": 12, "m": 13, "n": 14, "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20, "u":21, "v":22, "w": 23, "x": 24, "y": 25, "z": 26}
+    string_to_nums = []
+    
+    for letter in text:
+        if letter.lower() in my_dict.keys():
+            string_to_nums.append(my_dict.get(letter.lower()))
+        else:
+            continue
+    print(' '.join([str(i) for i in string_to_nums]))
+    return ' '.join([str(i) for i in string_to_nums])
+ 
+ """
+ kyu 7. INSTRUCTIONS. An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. 
+ Assume the empty string is an isogram. Ignore letter case.
+ """
+ 
+ def is_isogram(string):
+    string_lowered = string.lower()
+    counter = 0
+    for letter in string_lowered:
+        counter += string_lowered.count(letter)
+    if counter > len(string):
+        return False
+    else:
+        return True
+ 
+ 
