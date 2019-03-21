90dc10a0ea0c37e764ed7c67351bf75ce3bbe63d
iBilbo <35370175+iBilbo@users.noreply.github.com>
2019-03-21 11:15:40 +0000

    Update vasya-clerk.py

--- vasya-clerk.py
+++ vasya-clerk.py
@@ -1,5 +1,5 @@
 """
-The new "Avengers" movie has just been released! There are a lot of people at the cinema box office standing in a huge line. Each of them has a single 100, 50 or 25 dollars bill. An "Avengers" ticket costs 25 dollars.
+kyu6. The new "Avengers" movie has just been released! There are a lot of people at the cinema box office standing in a huge line. Each of them has a single 100, 50 or 25 dollars bill. An "Avengers" ticket costs 25 dollars.
 
 Vasya is currently working as a clerk. He wants to sell a ticket to every single person in this line.
 
