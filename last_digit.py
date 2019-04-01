9b225476ae41abd4cc67b9b80695abfe284634ca
iBilbo <35370175+iBilbo@users.noreply.github.com>
2019-04-01 11:48:15 +0100

    Create last_digit.py

---
+++ last_digit.py
@@ -0,0 +1,7 @@
+"""
+Define a function that takes in two non-negative integers a and b and returns the last decimal digit of a^b. Note that a and b may be very large!
+For example, the last decimal digit of 9^7 is 9, since 9^7 = 4782969. The last decimal digit of (2^200)^(2^300), which has over 10^92 decimal digits, is 6. Also, please take 0^0 to be 1.
+You may assume that the input will always be valid.
+"""
+def last_digit(n1, n2):
+    return pow(n1, n2, 10)
